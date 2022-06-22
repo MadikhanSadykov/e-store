6032a07f84f47ac5a26e4831aefb17e803dc842b
Madikhan Sadykov <madikhansadykov@gmail.com>
2022-06-22 20:23:44 +0500

    Heroku Postgres connecting

--- src/test/java/TestDataGenerator.java
+++ src/test/java/TestDataGenerator.java
@@ -66,7 +66,7 @@ public class TestDataGenerator {
         List<Category> categories = loadCategories();
         List<Producer> producers = getProducers(categories);
         clearMediaDir();
-        try (Connection c = DriverManager.getConnection(JDBC_URL, JDBC_USERNAME, JDBC_PASSWORD)) {
+        try (Connection c = DriverManager.getConnection(JDBC_URL)) {
             c.setAutoCommit(false);
             clearDb(c);
 
